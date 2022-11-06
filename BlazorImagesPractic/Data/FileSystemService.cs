fd63cf50e504edcd1921b1ae296bded642fa3d27
dobriy-yashcher <golovinsergey17@gmail.com>
2022-11-06 19:50:32 +0300

    add FileSystemService.

---
+++ BlazorImagesPractic/Data/FileSystemService.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver.GridFS;
+using MongoDB.Driver;
+
+namespace BlazorImagesPractic.Data
+{
+    public class FileSystemService
+    {
+        public async Task UploadImageToDbAsync(Stream fs, string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ImagesPractic");
+            var gridFS = new GridFSBucket(database);
+
+            await gridFS.UploadFromStreamAsync(name, fs);
+        }
+
+        public string DownloadToLocal(string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ImagesPractic");
+            var gridFS = new GridFSBucket(database);
+            using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/wwwroot/Images/")}{name}", FileMode.OpenOrCreate))
+            {
+                gridFS.DownloadToStreamByName(name, fs);
+            }
+
+            return $"Images/{name}";
+        }
+
+        public List<string> GetNamesOfImages()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images");
+            var collection = database.GetCollection<GridFSFileInfo>("fs.files");
+
+            List<string> names = new();
+            var images = collection.Find(x => x.Filename != null).ToList<GridFSFileInfo>();
+
+            foreach (var image in images) names.Add(image.Filename);
+
+            return names;
+        }
+    }
+}
